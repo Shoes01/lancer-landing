571295a1bb4052aacae56b3e86c3e5d5d85b83ea
Jason Gilbert <0jason.gilbert@gmail.com>
2021-07-31 21:25:53 -0300

    Begin adding tabs to swtich windows

--- Main.gd
+++ Main.gd
@@ -1,32 +1,31 @@
 extends Panel
 
 
-onready var MissionLog := $VBoxContainer/HBoxContainer/MissionLog
-
-onready var goals := $VBoxContainer/HBoxContainer/MissionLog/VBoxContainer/WhitePanel/VBoxContainer/GoalsBody
-onready var mission := $VBoxContainer/HBoxContainer/MissionLog/VBoxContainer/WhitePanel/VBoxContainer/MissionBody
-onready var mission_number := $VBoxContainer/HBoxContainer/MissionLog/VBoxContainer/WhitePanel/VBoxContainer/Mission
-onready var reserves := $VBoxContainer/HBoxContainer/MissionLog/VBoxContainer/WhitePanel/VBoxContainer/ReservesBody
-onready var stakes := $VBoxContainer/HBoxContainer/MissionLog/VBoxContainer/WhitePanel/VBoxContainer/StakesBody
-
+onready var control_panels := $VBoxContainer/Body/BodyInside/ControlPanels
 onready var planet_animation := $VBoxContainer/TopBar/Planet/Planet/AnimationPlayer
 
 
-func _ready():
-	mission_number.set_text("MISSION // #001")
-	var text := "Disrupt Harrison Armory reclamation efforts"
-	mission.set_text(text.to_upper())
-	goals.set_text(">>> Prevent Harrison Armory from establishing a base.")
-	stakes.set_text(">>> Upon failure, Harrison Armory will hunt you down.")
-	reserves.set_text(">>> COMP/CON Integrated Hacking Suite")
-	
-	
+func _ready() -> void:
 	planet_animation.play("Idle")
 
 
-func _on_Button_pressed():
+func _on_Button_pressed() -> void:
 	OS.shell_open("https://compcon.app/#/")
 
 
-func _on_Button2_pressed():
+func _on_Button2_pressed() -> void:
 	OS.shell_open("https://www.owlbear.rodeo/")
+
+
+func _on_ButtonMission_pressed() -> void:
+	for child in control_panels.get_children():
+		child.set_visible(false)
+	
+	#control_panels.get_node("MissionLog").set_visible(true)
+
+
+func _on_ButtonPilots_pressed() -> void:
+	for child in control_panels.get_children():
+		child.set_visible(false)
+	
+	control_panels.get_node("PilotRoster").set_visible(true)
